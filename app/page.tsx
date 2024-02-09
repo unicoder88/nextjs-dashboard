3c7995593f93d6ad872e760ab13322e3c987e03e
Roman Kabanov <roman@pearlconvert.no>
2024-02-09 14:04:10 +0100

    Try CSS modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import styles from '@/app/ui/home.module.css';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -13,6 +14,7 @@ export default function Page() {
           <div
               className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
           />
+          <div className={styles.shape} />
 
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
